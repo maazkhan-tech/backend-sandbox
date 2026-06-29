717caa01add9a8870905b170f87854d1d4beae30
Maaz Khan <maazkhan.expert@gmail.com>
2026-06-29 22:05:13 +0500

    Set up Express.js framework and convert existing APIs to Express.js

--- index.js
+++ index.js
@@ -1,59 +1,20 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 const PORT = 3000;
 
-const myServer = http.createServer((req, res) => {
-  const baseUrl = "http://" + req.headers.host || "localhost";
-  const fullUrl = new URL(req.url, baseUrl);
-  switch (fullUrl.pathname) {
-    case "/":
-      const logEntry = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-      fs.appendFile("server.log", logEntry, (err) => {
-        if (err) {
-          console.error("Error writing to log file:", err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.end("Welcome to the Home Page!");
-        }
-      });
-
-      break;
-    case "/about":
-      const logEntry2 = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-      fs.appendFile("server.log", logEntry2, (err) => {
-        if (err) {
-          console.error("Error writing to log file:", err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/plain" });
-        }
-      });
-
-      break;
-    case "/contact":
-      const logEntry3 = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-      fs.appendFile("server.log", logEntry3, (err) => {
-        if (err) {
-          console.error("Error writing to log file:", err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/plain" });
-          res.end("This is the Contact Page.");
-        }
-      });
-
-      break;
-    default:
-      const logEntry4 = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
-      fs.appendFile("server.log", logEntry4, (err) => {
-        if (err) {
-          console.error("Error writing to log file:", err);
-        } else {
-          res.writeHead(404, { "Content-Type": "text/plain" });
-          res.end("404 Not Found");
-        }
-      });
-  }
+app.get("/", (req, res) => {
+  const name = req.query.name || "Guest";
+  res.send(`Hello, ${name}! This is the Home Page!`);
 });
 
-myServer.listen(PORT, () => {
+app.get("/about", (req, res) => {
+  res.send("This is the About Page!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("This is the Contact Page!");
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
